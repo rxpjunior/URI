b222357cec71f0ecbba4a4c26f2c31d841e80fb5
rxpjunior <rxpjunior@gmail.com>
2021-04-04 16:46:59 -0300

    Nova tentativa exercicio 1047 sem suceso

--- 1047.py
+++ 1047.py
@@ -8,17 +8,21 @@ mFim = int(aux[3])
 # Calculo das horas
 if hInicio < hFim:
     hora = hFim - hInicio
+if hFim == (hInicio + 1) and mInicio > mFim: # Contempla casos onde jogos duraram apenas minutos e horas diferentes
+    hora = 0 
+elif hFim == hInicio and mInicio > mFim:
+    hora = 23
 elif hInicio == hFim:
     hora = 24
 else:
     hora = (hFim + 12) - (hInicio - 12)
 
 # Calculo dos minutos
-minuto = 0
-# continuar calculo minutos
-
-# Contempla casos onde jogos duraram apenas minutos e horas diferentes
-if hFim == (hInicio + 1) and mInicio > mFim:
-    hora = 0 
+if mFim > mInicio:
+    minuto = mFim - mInicio
+elif mFim == mInicio:
+    minuto = 0
+else:
+    minuto = 60 - mInicio + mFim
 
 print(f"O JOGO DUROU {hora} HORA(S) e {minuto} MINUTO(S)")
