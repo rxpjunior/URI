632ad91bfc27cd16aa3232f3af634c8a66976914
rxpjunior <rxpjunior@gmail.com>
2021-04-28 21:49:51 -0300

    Inicio problema 1061

---
+++ Iniciante/1061.py
@@ -0,0 +1,10 @@
+diaInicio = input()
+horaInicio = input()
+diaFim = input()
+horaFim = input()
+aux = horaInicio.split(" : ")
+a, b, c = aux
+aux = horaFim.split(" : ")
+d, e, f = aux
+print(a,"-",b,"-",c,"-")
+print(d,"-",e,"-",f,"-")
