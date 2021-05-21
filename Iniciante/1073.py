46acfa81177c427c4d052b536f424fdb9378a96b
Your Namerxpjunior <rxpjunior@gmail.com>
2021-05-21 15:27:18 -0300

    Problemas 1072 e 1073

---
+++ Iniciante/1073.py
@@ -0,0 +1,18 @@
+"""
+Leia um valor inteiro N. Apresente o quadrado de cada um dos valores pares, de 1 até N, inclusive N, se for o caso.
+
+Entrada
+A entrada contém um valor inteiro N (5 < N < 2000).
+
+Saída
+Imprima o quadrado de cada um dos valores pares, de 1 até N, conforme o exemplo abaixo.
+
+Tome cuidado! Algumas linguagens tem por padrão apresentarem como saída 1e+006 ao invés de 1000000 o que ocasionará resposta errada. Neste caso, configure a precisão adequadamente para que isso não ocorra.
+"""
+
+entrada = int(input())
+for x in range(1, (entrada+1)):
+    if x % 2 == 0:
+        print(f"{x}^{2} = {x**2:.0f}")
+
+
