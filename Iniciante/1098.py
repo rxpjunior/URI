5869b8273633d8d8fa4364254150a6a7d6c2f8e2
Your Namerxpjunior <rxpjunior@gmail.com>
2021-06-16 16:31:42 -0300

    Problema 1098 Versao preliminar

---
+++ Iniciante/1098.py
@@ -0,0 +1,7 @@
+x = 0
+while x <= 2:
+    for z in range(3):
+        print(f'X={x:.1f} J={x+z+1:.1f}')
+    x += 0.2
+
+       
--- Iniciante/teste.py
+++ Iniciante/teste.py
@@ -1,2 +1,20 @@
 for x in range(3):
     print(x)
+
+if 10 == type(int):
+    print("int")
+    print(type(int))
+else:
+    print("nao")
+    print(type(int))
+
+
+f = 1.23
+
+print(f.is_integer())
+# False
+
+f_i = 100.0
+s = f_i.is_integer()
+print(s)
+# True
